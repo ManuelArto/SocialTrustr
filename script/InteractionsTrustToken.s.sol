ec0eb20a64354ec8705c43191b9a2e95e4e06941
ManuelArto <43347768+ManuelArto@users.noreply.github.com>
2023-11-15 14:15:10 +0100

    Add transferTrs interaction method

--- script/InteractionsTrustToken.s.sol
+++ script/InteractionsTrustToken.s.sol
@@ -32,37 +32,54 @@ contract TrustTokenInteractions is Script {
         logInfos(trs, previousETH, previousTRS, fundTRS, contractBalance);
     }
 
-    function sellTrustToken(uint amount) external startBroadcast {
+    function transferTrs(address _to, uint amount) public startBroadcast {
         TrustToken trs = getTrustTokenContract();
-        uint previousETH = msg.sender.balance;
-        uint previousTRS = trs.balanceOf(msg.sender);
-        uint fundTRS = trs.getFundsTRS();
-        uint contractBalance = address(trs).balance;
+        if (_to == address(0)) {
+            _to = address(trs);
+        }
 
-        trs.transfer(address(trs), amount);
-        logInfos(trs, previousETH, previousTRS, fundTRS, contractBalance);
+        uint myPrevETH = msg.sender.balance;
+        uint myPrevTRS = trs.balanceOf(msg.sender);
+        uint toPrevTRS = trs.balanceOf(_to);
+        uint toPrevBalance = _to.balance;
+
+        trs.transfer(_to, amount);
+        logInfos(trs, myPrevETH, myPrevTRS, toPrevTRS, toPrevBalance, _to);
     }
 
     function getInfos() external {
         getInfos(msg.sender);
     }
 
+    function sellTrustToken(uint amount) external {
+        transferTrs(address(0), amount);
+    }
+
     function getInfos(address _address) public startBroadcast {
         TrustToken trs = getTrustTokenContract();
+        console.log("----------------------------------------------------");
         console.log("TRS: %s", trs.balanceOf(_address));
         console.log("ETH: %s", _address.balance);
         console.log("TrustLevel : %s", trs.getTrustLevel(_address));
+        console.log("----------------------------------------------------");
         console.log("Contract ETH: %s", address(trs).balance);
         console.log("Contract TRS: %s", trs.getFundsTRS());
+        console.log("----------------------------------------------------");
+        console.log("TrustedUsers: %s", trs.trustedUsers());
     }
 
-    function logInfos(TrustToken trs, uint previousETH, uint previousTRS, uint fundTRS, uint contractBalance) internal view {
+    function logInfos(TrustToken trs, uint previousETH, uint previousTRS, uint fundTRS, uint addressBalance, address _to) internal view {
         console.log("----------------------------------------------------");
         console.log("My ETH: %s => %s", previousETH, msg.sender.balance);
         console.log("My TRS: %s => %s", previousTRS, trs.balanceOf(msg.sender));
         console.log("----------------------------------------------------");
-        console.log("Contract ETH: %s => %s", contractBalance, address(trs).balance);
-        console.log("Contract TRS: %s => %s", fundTRS, trs.getFundsTRS());
+        string memory toText = _to == address(trs) ? "Contract" : "Address";
+        console.log("%s ETH: %s => %s", toText, addressBalance, _to.balance);
+        console.log("%s TRS: %s => %s", toText, fundTRS, trs.balanceOf(_to));
+    }
+
+    function logInfos(TrustToken trs, uint previousETH, uint previousTRS, uint fundTRS, uint addressBalance) internal view {
+        logInfos(trs, previousETH, previousTRS, fundTRS, addressBalance, address(trs));
     }
 
     function getTrustTokenContract() internal returns (TrustToken) {
